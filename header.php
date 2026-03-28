e43d9f8dab65ae7ffae2965e31aba88c6683b831
Ali Raza Mujahid <razadeveloper816@gmail.com>
2026-03-28 09:35:44 -0700

    complete header design

--- header.php
+++ header.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration - Registration Page</title>
+    <!-- stylesheet -->
+    <link rel="stylesheet" href="header.css">
 </head>
 <body>
     <header>
-        <h1>Registration</h1>
-        <div>
-            <button>Sign Up</button>
-            <button>Login</button>
+        <h1>Page</h1>
+        <div class="buttons">
+            <button><a href="">Sign Up</a></button>
+            <button><a href="">Login</a></button>
         </div>
     </header>
 </body>
