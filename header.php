67ea692cf338a3a4f39c2c9eb7fafd9897d5525c
Ali Raza Mujahid <razadeveloper816@gmail.com>
2026-03-28 09:18:52 -0700

    add header & index file

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration - Registration Page</title>
+</head>
+<body>
+    <header>
+        <h1>Registration</h1>
+        <div>
+            <button>Sign Up</button>
+            <button>Login</button>
+        </div>
+    </header>
+</body>
+</html>
